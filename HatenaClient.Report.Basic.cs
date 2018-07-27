380581560abe9cb92a68dfffb8c0e4b5f6eaec24
suihan <suihan742@gmail.com>
2018-07-28 01:39:47 +0900

    Add a method for reporting annoying users

---
+++ HatenaClient.Report.Basic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatenaLib
+{
+    public enum ReportCategory
+    {
+        Spam = 0,
+        Slander,
+        PrivacyInfringement,
+        CrimeNotice,
+        Discrimination,
+        Other
+    }
+
+    public partial class HatenaClient
+    {
+        public async Task Report(Entities.Entry entry, string userName, ReportCategory category, string comment = "")
+        {
+            var url = $"{BaseUrl}/entry.report";
+            var contents = new Dictionary<string, string>
+            {
+                {"rks", RksForBookmark},
+                {"eid", entry.Id.ToString()},
+                {"name", userName},
+                {"category", category.GetHashCode().ToString()},
+                {"comment", comment}
+            };
+            await PostAsync(url, contents, GetCookieHeader());
+        }
+    }
+}
